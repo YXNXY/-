433f2246403f830bc38626a6c357b97e84a9e9b0
YXNXY <197541189@qq.com>
2026-09-19 11:38:30 +0800

    Add files via upload

---
+++ 454. 四数相加 II.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        unordered_map<int,int> mp;
+        int ans=0;
+        for(int x:nums1){
+            for(int y:nums2){
+                mp[x+y]++;
+            }
+        }
+        for(int x:nums3){
+            for(int y:nums4){
+                ans+=mp[-(x+y)];
+            }
+        }
+        return ans;
+    }
+};
